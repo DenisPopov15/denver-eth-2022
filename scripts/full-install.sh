0fa07cc433ba4effc1faadcaa9a4ecd0fcbd0a0c
Dmytro Filipenko <dmfilipenko@MacBook-Pro-35.fritz.box>
2022-02-13 14:38:49 +0100

    process

--- scripts/full-install.sh
+++ scripts/full-install.sh
@@ -2,5 +2,6 @@
 
 npm --prefix ./connectors/sourcecred/instance install  
 npm --prefix ./connectors/coordinApe install 
+npm --prefix ./connectors/poap install 
 npm --prefix ./connectors/github install 
 npm --prefix ./connectors/sourcecred/server install 
