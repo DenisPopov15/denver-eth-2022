ce8bcc21d66532a347113020fe994aead9f779d8
Dmytro Filipenko <wind.fd@gmail.com>
2022-02-15 12:26:44 +0100

    connectors

---
+++ connectors-frontend/pages/connectors.js
@@ -0,0 +1,23 @@
+export default function Connectors() {
+  return (
+    <div className="container">
+      <ul>
+        <li>
+          <button>coordinApe</button>
+        </li>
+        <li>
+          <button>discord</button>
+        </li>
+        <li>
+          <button>github</button>
+        </li>
+        <li>
+          <button>poap</button>
+        </li>
+        <li>
+          <button>sourceCred</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
---
+++ connectors-frontend/services/metamask.js
@@ -0,0 +1,5 @@
+import { ethers } from "ethers"
+
+const requestAccounts = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum)
+}
