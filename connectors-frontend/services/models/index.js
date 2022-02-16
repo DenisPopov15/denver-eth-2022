c5850ec3df928e00f50f27ec9063504849088c08
Denis <denis@slatestudio.com>
2022-02-16 20:13:51 +0200

    Add basic logic for Ceramic pulling data, etc

---
+++ connectors-frontend/services/models/index.js
@@ -0,0 +1,15 @@
+import colonies from '@glazed/datamodel'
+import apeprofiles from './apeprofiles'
+import discords from './discords'
+import githubs from './githubs'
+import poaps from './poaps'
+import sourcecreds from './sourcecreds'
+
+export {
+  colonies,
+  apeprofiles,
+  discords,
+  githubs,
+  poaps,
+  sourcecreds,
+}
