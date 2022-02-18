f15a92599eb384922650ff7ffc77a3479a7c4c1d
Dmytro Filipenko <wind.fd@gmail.com>
2022-02-18 12:21:04 +0100

    path

--- connectors-frontend/pages/api/sourcecred.js
+++ connectors-frontend/pages/api/sourcecred.js
@@ -1,3 +1,3 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const handler = require('../connectors/sourcecred/api')
+const handler = require('../../connectors/sourcecred/api')
 export default handler
